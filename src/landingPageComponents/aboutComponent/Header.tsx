3935ff684bbc543ec60315e468d38efbd18248a5
sleek-ennju <ihenachochukwudi@gmail.com>
2024-07-28 23:10:27 +0100

    adjusted hover color on the header section in the about component

--- src/landingPageComponents/aboutComponent/Header.tsx
+++ src/landingPageComponents/aboutComponent/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC<landingPageRouteHeader> = ({routeTitle, routeName}) => {
     <section className="bg-[#F2F2F2] h-[86dvh] flex flex-col items-center justify-center gap-2">
         <h1 className="text-[#333333] text-[1.625rem] md:text-5xl">{routeTitle}</h1>
         <div className="flex items-center gap-2 text-[#ababab] text-base md:text-lg">
-          <h2 className="hover:text-blue-600 transition-all duration-300">
+          <h2 className="hover:text-[#1054c8] transition-all duration-300">
             <Link to={"/"}>Home</Link>
           </h2>
           <span>/</span>
